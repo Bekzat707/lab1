72ecf2a6abddf00c5ca65f86776c8e94742b504d
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 16:31:05 +0600

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,31 @@
+#task1
+x = "Hello World"
+print(len(x))
+
+#task2
+txt = "Hello World"
+x = txt[0]
+
+#task3
+txt = "Hello World"
+x =txt[2:5] 
+
+#task4
+txt.strip()
+
+#task5
+txt = "Hello World"
+txt = txt.upper()
+
+#task6
+txt = "Hello World"
+txt = txt.lower()
+
+#task7
+txt = "Hello World"
+txt = txt.replace("H","J")
+
+#task8
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
