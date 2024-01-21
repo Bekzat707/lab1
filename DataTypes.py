7263ea4df3f6c04915af4189aad43c5696ca522b
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 16:30:58 +0600

    Create DataTypes.py

---
+++ DataTypes.py
@@ -0,0 +1,34 @@
+#task1
+x = 5
+print(type(x))
+#int
+
+#task2
+x = "Hello World"
+print(type(x))
+#str
+
+#task3
+x = 20.5
+print(type(x))
+#float
+
+#task4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#list
+
+#task5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#tuple
+
+#task6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#dict
+
+#task7
+x = True
+print(type(x))
+#bool
