2e9d4be8291233af53d3e0754884c371b4ad4e25
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-01-21 16:31:08 +0600

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,29 @@
+#task1
+carname="Volvo"
+
+#task2
+x=50
+
+#task3
+x=5
+y=10
+print(x+y)
+
+#task4
+x=5
+y=10
+z=x+y
+print(z)
+
+#task5
+x,y,z = "Orange", "Banana", "Cherry"
+
+#task6
+x=y=z="Orange"
+
+#task7
+def myfunc():
+  global x
+  x = "fantastic"
+  
+
